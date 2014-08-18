4a28cd83902ddc2d074cbed1e817c472bf30b056
LostMekka <lostmekka@web.de>
2014-08-19 00:52:47 +0200

    levels will not get bigger that fast

--- src/hfk/game/GameController.java
+++ src/hfk/game/GameController.java
@@ -275,7 +275,7 @@ public class GameController {
 		shots.clear();
 		texts.clear();
 		levelCount++;
-		int s = 25 + 2*levelCount;
+		int s = 25 + levelCount;
 		int d = getLevelDifficultyLimit(levelCount);
 		int r = getLevelRarityLimit(levelCount);
 		level = Level.Factory.createTestArena(s, s, d, r);
