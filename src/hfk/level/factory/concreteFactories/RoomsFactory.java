a61cd6a749371a7383bd236b65830e27f45e4dae
LostMekka <lostmekka@web.de>
2014-12-14 01:32:39 +0100

    made crates even more likely to spawn added cryo room to rooms generator (currently disabled until it looks better)

--- src/hfk/level/factory/concreteFactories/RoomsFactory.java
+++ src/hfk/level/factory/concreteFactories/RoomsFactory.java
@@ -10,6 +10,7 @@ import hfk.level.factory.LevelFactory;
 import hfk.level.factory.PropertyMap;
 import hfk.level.factory.generators.BorderGenerator;
 import hfk.level.factory.generators.CrateGenerator;
+import hfk.level.factory.generators.CryoChamberGenerator;
 import hfk.level.factory.generators.RandomPrimitiveGenerator;
 import hfk.level.factory.generators.RoomsGenerator;
 
@@ -19,17 +20,21 @@ import hfk.level.factory.generators.RoomsGenerator;
  */
 public class RoomsFactory extends LevelFactory{
 
-	private final RandomPrimitiveGenerator floor;
+	private final RandomPrimitiveGenerator floor, floorCryo;
 	private final RandomPrimitiveGenerator walls;
 	private final CrateGenerator crates;
 	private final RoomsGenerator rooms;
 	private final BorderGenerator border;
+	private final CryoChamberGenerator cryo;
 	
 	public RoomsFactory(int width, int height) {
 		super(width, height);
 		floor = new RandomPrimitiveGenerator(RandomPrimitiveGenerator.Type.floor, this);
 		floor.addPrimitiveTileType(0, 0, 0, 1f);
 		floor.addPrimitiveTileType(0, 0, 1, 0.1f);
+		floorCryo = new RandomPrimitiveGenerator(RandomPrimitiveGenerator.Type.floor, this);
+		floorCryo.addPrimitiveTileType(0, 1, 0, 1f);
+		floorCryo.addPrimitiveTileType(0, 1, 1, 0.1f);
 		walls = new RandomPrimitiveGenerator(RandomPrimitiveGenerator.Type.wall, this);
 		walls.addPrimitiveTileType(0, 0, 0, 1f);
 		walls.addPrimitiveTileType(0, 0, 1, 0.1f);
@@ -44,14 +49,19 @@ public class RoomsFactory extends LevelFactory{
 		crates.mapMaxX = PropertyMap.createRandom(width, height, 7, 1, 2f, 4.2f);
 		crates.mapMinY = PropertyMap.createRandom(width, height, 7, 1, 1.2f, 3f);
 		crates.mapMaxY = PropertyMap.createRandom(width, height, 7, 1, 2f, 4.2f);
-		crates.boxProbability = PropertyMap.createRandom(width, height, 7, 1, 0f, 0.4f);
+		crates.boxProbability = PropertyMap.createRandom(width, height, 7, 1, -0.2f, 0.7f);
+		cryo = new CryoChamberGenerator(this);
+		cryo.generateDefaultPropertyMaps(width, height);
+		cryo.floor1 = floorCryo;
+		cryo.floor2 = floor;
 		rooms = new RoomsGenerator(this);
 		rooms.generateDefaultPropertyMaps(width, height);
 		rooms.empty = floor;
 		rooms.floors = floor;
 		rooms.walls = walls;
 		rooms.addInnerGenerator(floor, 1f);
-		rooms.addInnerGenerator(crates, 2f);
+		rooms.addInnerGenerator(crates, 40f);
+		//rooms.addInnerGenerator(cryo, 3f);
 		border = new BorderGenerator(LEVEL_BORDER, this);
 		border.floor = floor;
 		border.walls = walls;
