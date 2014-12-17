79d3470c61549497a4b7250e3f572c85d2a70b42
LostMekka <lostmekka@web.de>
2014-12-17 17:10:12 +0100

    debugged cave generator. it works now \o/ now we just need some suitable cave textures...

--- src/hfk/level/factory/generators/CaveGenerator.java
+++ src/hfk/level/factory/generators/CaveGenerator.java
@@ -1,4 +1,4 @@
-package hfk.level.factory.generators;
+										package hfk.level.factory.generators;
 
 import hfk.Box;
 import hfk.PointCloud;
@@ -8,10 +8,16 @@ import hfk.game.GameController;
 import hfk.level.Level;
 import hfk.level.factory.LevelGenerator;
 import hfk.level.factory.PropertyMap;
+import java.awt.Color;
+import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.LinkedList;
 import java.util.Random;
+import javax.imageio.ImageIO;
 
 /**
  *
@@ -46,8 +52,8 @@ public class CaveGenerator extends LevelGenerator {
 
 	@Override
 	public void generateDefaultPropertyMaps(int width, int height) {
-		wallMap = PropertyMap.createRandom(width, height, 7, 1, -1.4f, 1f);
-		wallMap.fadeOutEllypse(0.3f, 1f, 1f);
+		wallMap = PropertyMap.createRandom(width, height, 6, 1, -1.2f, 1f);
+		wallMap.fadeOutEllypse(0.75f, 1f, 1f);
 	}
 	
 	private class Connection{
@@ -71,7 +77,7 @@ public class CaveGenerator extends LevelGenerator {
 		PointI p = new PointI();
 		for(p.x=0; p.x<l.getWidth(); p.x++){
 			for(p.y=0; p.y<l.getHeight(); p.y++){
-				PointCloud pc = getArea(p, marks, isFloor);
+				PointCloud pc = getArea(p.clone(), marks, isFloor);
 				if(pc != null){
 					areas.add(pc);
 					totalSize += pc.getPointCount();
@@ -88,10 +94,10 @@ public class CaveGenerator extends LevelGenerator {
 		// remove smallest areas as long as we have enough space left to spare
 		int minSize = (int)(l.getWidth() * l.getHeight() * minFloorRatio);
 		while(areas.size() > 1){
-			int size = areas.getLast().getPointCount();
-			if(totalSize - size < minSize) break;
+			int size = areas.getFirst().getPointCount();
+			if(totalSize - size < minSize && size > 9) break;
 			totalSize -= size;
-			PointCloud pc = areas.removeLast();
+			PointCloud pc = areas.removeFirst();
 			mark(marks, pc, false);
 			mark(isFloor, pc, false);
 			if(totalSize == minSize) break;
@@ -103,10 +109,7 @@ public class CaveGenerator extends LevelGenerator {
 		while(!areas.isEmpty()){
 			LinkedList<Connection> valid = new LinkedList<>();
 			for(Connection c : connections) if(connectsToOneOf(c, connectedAreas)) valid.add(c);
-			if(valid.isEmpty()){
-				int i=0;
-			}
-			Connection c = valid.get(r.nextInt(valid.size()+1));
+			Connection c = valid.get(r.nextInt(valid.size()));
 			connections.remove(c);
 			markTunnel(isFloor, isTunnel, c.path);
 			PointCloud a = connectedAreas.contains(c.a1) ? c.a2 : c.a1;
@@ -138,20 +141,47 @@ public class CaveGenerator extends LevelGenerator {
 			}
 		}
 		// let the inner generators do all the dirty work
-		walls.generate(l, wallArea);
 		floor.generate(l, floorArea);
+		floor.generate(l, wallArea);
+		walls.generate(l, wallArea);
 		tunnelFloor.generate(l, tunnelArea);
+		tunnelFloor.generate(l, tunnelWallArea);
 		tunnelWalls.generate(l, tunnelWallArea);
 		lastSpawnArea = floorArea;
 	}
 	
+	private static final Color[] DEBUG_COLORS = {
+		Color.BLACK, Color.RED, Color.BLUE, Color.ORANGE, Color.YELLOW, Color.PINK, Color.DARK_GRAY, Color.GREEN, Color.MAGENTA};
+	private BufferedImage drawDebug(LinkedList<PointCloud> areas, String name, BufferedImage i, Color c){
+		int pix = 10;
+		boolean newI = i == null;
+		if(newI) i = new BufferedImage(getLevelSize().x*pix, getLevelSize().y*pix, BufferedImage.TYPE_INT_ARGB);
+		Graphics2D g = i.createGraphics();
+		g.setColor(Color.white);
+		if(newI) g.fillRect(0, 0, getLevelSize().x * pix, getLevelSize().y * pix);
+		int count = 0;
+		for(PointCloud pc : areas){
+			g.setColor(c != null ? c : DEBUG_COLORS[count % DEBUG_COLORS.length]);
+			count++;
+			for(PointI p : pc){
+				g.fillRect(p.x*pix, p.y*pix, pix, pix);
+			}
+		}
+		g.dispose();
+		try{
+			ImageIO.write(i, "png", new File("RoomsDebug_" + name + ".png"));
+		} catch(IOException e){}
+		return i;
+	}
+	
 	private void mark(boolean[][] a, PointCloud s, boolean val){
-		for(PointI p : s) a[p.x][p.y] = val;
+		for(PointI p : s) if(p.x>=0 && p.y>=0 && p.x<a.length && p.y<a[0].length) a[p.x][p.y] = val;
 	}
 	
 	private void markTunnel(boolean[][] isFloor, boolean[][] isTunnel, PointCloud path){
 		for(PointI p : path){
 			isFloor[p.x][p.y] = true;
+			isTunnel[p.x][p.y] = true;
 			if(!isFloor[p.x+1][p.y]) isTunnel[p.x+1][p.y] = true;
 			if(!isFloor[p.x-1][p.y]) isTunnel[p.x-1][p.y] = true;
 			if(!isFloor[p.x][p.y+1]) isTunnel[p.x][p.y+1] = true;
@@ -168,17 +198,24 @@ public class CaveGenerator extends LevelGenerator {
 		LinkedList<ConnectNode> toExpand = getNodes(areas, marks);
 		while(!toExpand.isEmpty()){
 			ConnectNode n = toExpand.removeFirst();
-			// make sure that the first direction to expand is the same as last time
-			int dx = n.loc.x - n.last.loc.x;
-			int dy = n.loc.y - n.last.loc.y;
-			PointI[] pa = new PointI[]{new PointI(n.loc), new PointI(n.loc), new PointI(n.loc)};
-			if(dx != 0){
-				pa[0].x += dx; pa[1].y++; pa[2].y--;
+			PointI[] pa;
+			if(n.last == null){
+				// this is the first node of the possible tunnel
+				pa = new PointI[]{new PointI(n.loc), new PointI(n.loc), new PointI(n.loc), new PointI(n.loc)};
+				pa[0].x++; pa[1].x--; pa[2].y++; pa[3].y--;
+			} else {
+				// make sure that the first direction to expand is the same as last time
+				int dx = n.loc.x - n.last.loc.x;
+				int dy = n.loc.y - n.last.loc.y;
+				pa = new PointI[]{new PointI(n.loc), new PointI(n.loc), new PointI(n.loc)};
+				if(dx != 0){
+					pa[0].x += dx; pa[1].y++; pa[2].y--;
+				}
+				if(dy != 0){
+					pa[0].y += dy; pa[1].x++; pa[2].x--;
+				}
 			}
-			if(dy != 0){
-				pa[0].y += dy; pa[1].x++; pa[2].x--;
-			}
-			for(PointI pe : pa) if(wallMap.getFloatAt(pe) < 0f){
+			for(PointI pe : pa) if(wallMap.getFloatAt(pe) >= 0f){
 				if(marks[pe.x][pe.y]){
 					// connection found! is it the first for this area pair?
 					ConnectNode n2 = getNode(pe, toExpand);
@@ -208,6 +245,7 @@ public class CaveGenerator extends LevelGenerator {
 	}
 	
 	private boolean ConnectionExists(PointCloud a1, PointCloud a2, LinkedList<Connection> connections){
+		if(a1 == a2) return true;
 		for(Connection c : connections){
 			if((c.a1 == a1 && c.a2 == a2) || (c.a1 == a2 && c.a2 == a1)){
 				return true;
@@ -228,8 +266,8 @@ public class CaveGenerator extends LevelGenerator {
 			pa[0].x++; pa[1].x--; pa[2].y++; pa[3].y--;
 			for(PointI p : pa){
 				if(p.x<0 || p.y<0 || p.x>=marks.length || p.y>=marks[0].length) continue;
-				if(!marks[p.x][p.y] && wallMap.getFloatAt(p) < 0f){
-					nodes.add(new ConnectNode(p, null, c));
+				if(!marks[p.x][p.y] && wallMap.getFloatAt(p) >= 0f){
+					nodes.addLast(new ConnectNode(p, null, c));
 				}
 			}
 		}
