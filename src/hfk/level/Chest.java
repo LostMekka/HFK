816df1ff7ddcd2d1386464c2db01bc185d9e7422
LostMekka <lostmekka@web.de>
2015-04-02 20:53:23 +0200

    made chest destructable

--- src/hfk/level/Chest.java
+++ src/hfk/level/Chest.java
@@ -23,7 +23,7 @@ public class Chest extends UsableLevelItem {
 	public Chest(PointI pos) {
 		super(pos);
 		img = Resources.getImage("chest.png");
-		hp = -1;
+		hp = 70;
 		size.set(28f/32f, 20f/32f);
 		MobStatsCard card = MobStatsCard.createNormal();
 		card.setInventorySize(20);
@@ -31,6 +31,16 @@ public class Chest extends UsableLevelItem {
 		inv = new Inventory(card);
 		inv.addAmmo(Weapon.AmmoType.bullet, 333);
 		for(int i=0; i<10; i++) inv.addItem(InventoryItem.create(new PointF(), 99999999));
+	}
+
+	@Override
+	public boolean damage(int dmg) {
+		if(!super.damage(dmg)) return false;
+		for(InventoryItem i : inv.removeAll()){
+			i.pos = pos.toFloat();
+			GameController.get().dropItem(i, null, false);
+		}
+		return true;
 	}
 	
 	@Override
